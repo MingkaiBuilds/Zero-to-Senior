4bdf6400167155b2d4e1bb54877b6672daae735b
Mingkai <mingkai.builds@gmail.com>
2025-11-29 11:47:54 -0500

    I am gaining React consciousness

---
+++ frontend-react/playground/src/components/StudyInput.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+type StudyInputProps = {
+    onAdd: (name: string) => void;
+};
+
+export function StudyInput({ onAdd }: StudyInputProps) {
+    const [text, setText] = useState("");
+
+    return (
+        <div>
+            <input value={ text } onChange={(e) => setText(e.target.value)} />
+            <button onClick={() => {
+                if (text.trim().length === 0) return;
+                onAdd(text);
+                setText("");
+            }}>Add</button>
+        </div>
+    );
+}
