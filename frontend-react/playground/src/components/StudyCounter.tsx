eac635a51a61a832606e14c693d38fabd50a3600
Mingkai <mingkai.builds@gmail.com>
2025-11-28 23:57:54 -0500

    Figuring out React nonsense

---
+++ frontend-react/playground/src/components/StudyCounter.tsx
@@ -0,0 +1,14 @@
+import React , { useState } from "react"
+
+export function  StudyCounter() {
+    const [count, setCount] = useState(0)
+
+   return (
+    <div>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>
+            Increase Count
+        </button>
+    </div>
+   );
+}
