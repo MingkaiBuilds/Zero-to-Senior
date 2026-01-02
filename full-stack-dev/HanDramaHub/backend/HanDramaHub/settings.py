1ae506ca02058948d120fd3f054cfd8e711e8498
Mingkai <mingkai.builds@gmail.com>
2026-01-01 20:01:44 -0500

    Created a Drama model and handled ORM behaviour

--- full-stack-dev/HanDramaHub/backend/HanDramaHub/settings.py
+++ full-stack-dev/HanDramaHub/backend/HanDramaHub/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'HanDramaHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'handramahub_db',
+        'USER': 'handramahub_user',
+        'PASSWORD': 'test@12345',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
+
     }
 }
 
--- full-stack-dev/HanDramaHub/backend/core/models.py
+++ full-stack-dev/HanDramaHub/backend/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+STATUSES = [
+    ('P', 'Plan'),
+    ('W', 'Watching'),
+    ('C', 'Completed'),
+]
+
 # Create your models here.
+class Drama(models.Model):
+    #title
+    title = models.CharField(max_length=200)
+    #year
+    year = models.IntegerField()
+    #status
+    status = models.CharField(choices=STATUSES, default='plan')
+    #rating
+    rating = models.DecimalField(decimal_places=1, max_digits=3)
+    #created_at
+    created_at = models.DateTimeField(auto_now_add=True)
+    #updated_at
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f'{self.title} {self.status}'
+ 
